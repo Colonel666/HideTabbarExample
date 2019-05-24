4f83bc0604d6e5d64af3c0fb4bc75fe45ee7b51c
Jop Cobussen <info@jopcobussen.nl>
2019-05-24 13:12:13 +0200

    Add files via upload

---
+++ HideTabbarExample/HideTabbarExample/MyPageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace HideTabbarExample
+{
+    public class MyPageViewModel
+    {
+        public MyPageViewModel()
+        {
+            PushDetailPageWithoutTabbarCommand = new Command(() => PushWithoutNavbar());
+        }
+
+        public ICommand PushDetailPageWithoutTabbarCommand { get; set; }
+
+        void PushWithoutNavbar ()
+        {
+            App.Current.MainPage.Navigation.PushAsync(new MyPage());
+        }
+    }
+}
+
